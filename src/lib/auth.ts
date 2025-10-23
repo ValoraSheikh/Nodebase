6442016aa6f91f34af3f40b8f798a2ea21ee7483
Aman Sheikh <haarishsheikh04@gmail.com>
2025-10-23 10:18:44 +0530

    10: Payments

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,38 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/db";
+import {
+  polar,
+  checkout,
+  portal,
+} from "@polar-sh/better-auth";
+import { polarClient } from "./polar";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
-    provider: "postgresql"
+    provider: "postgresql",
   }),
   emailAndPassword: {
     enabled: true,
-    autoSignIn: true
-  }
+    autoSignIn: true,
+  },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "0923a93f-ce27-46b7-a0aa-4b81bc254833",
+              slug: "pro",
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+  ],
 });
