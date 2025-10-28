d982d2be696b5d5e621f367bef16750dafaf8093
Aman Sheikh <haarishsheikh04@gmail.com>
2025-10-28 11:35:55 +0530

    13: Workflow UI

--- src/features/workflows/hooks/use-workflows.ts
+++ src/features/workflows/hooks/use-workflows.ts
@@ -10,7 +10,7 @@ import { useWorkflowParams } from "./use-workflows-params";
 export const useSuspenseWorkflows = () => {
   const trpc = useTRPC();
   const [params] = useWorkflowParams();
-  
+
   return useSuspenseQuery(trpc.workflows.getMany.queryOptions(params));
 };
 
@@ -26,8 +26,28 @@ export const useCreateWorkflow = () => {
       },
 
       onError: (error) => {
-        toast.error(`Failed to create workflow: ${error.message}`)
-      }
+        toast.error(`Failed to create workflow: ${error.message}`);
+      },
+    })
+  );
+};
+
+export const useRemoveWorkflow = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    trpc.workflows.remove.mutationOptions({
+      onSuccess: (data) => {
+        toast.success(`Workflow "${data.name}" removed`);
+        queryClient.invalidateQueries(trpc.workflows.getMany.queryOptions({}));
+        queryClient.invalidateQueries(
+          trpc.workflows.getOne.queryOptions({ id: data.id })
+        );
+      },
+      onError: (error) => {
+        toast.error(`Failed to remove workflow: ${error.message}`);
+      },
     })
   );
 };
